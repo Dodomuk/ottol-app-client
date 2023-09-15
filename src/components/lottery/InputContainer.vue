2fd21422447dbd7844347b6e31a27ac275a654a9
정동묵 <siderid@naver.com>
2023-09-15 23:14:44 +0900

    scss 환경 제공

--- src/components/lottery/InputContainer.vue
+++ src/components/lottery/InputContainer.vue
@@ -14,46 +14,41 @@ const lottoNumbers = Array(45)
 
 onMounted(() => {})
 </script>
-<style>
+<style lang="scss">
 .container {
     width: 100px;
-}
-.number-wrapper {
-    position: relative;
-    display: inline-block;
-    width: 25px;
-    height: 47px;
-    border: 1px solid var(--main-color);
-    color: var(--main-color);
-    font-size: 12px;
-    font-weight: bold;
-    text-align: center;
-}
-
-.number-wrapper::before {
-    content: '';
-    position: absolute;
-    left: -2px;
-    top: 20%;
-    bottom: 20%;
-    width: 2px;
-    background-color: var(--white);
-}
-.number-wrapper::after {
-    content: '';
-    position: absolute;
-    right: -2px;
-    top: 20%;
-    bottom: 20%;
-    width: 2px;
-    background-color: var(--white);
-}
-
-.number {
-    margin: 0;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    .number-wrapper {
+        position: relative;
+        display: inline-block;
+        width: 25px;
+        height: 47px;
+        border: 1px solid var(--main-color);
+        color: var(--main-color);
+        font-size: 12px;
+        font-weight: bold;
+        text-align: center;
+        &::before,
+        &::after {
+            content: '';
+            position: absolute;
+            top: 20%;
+            bottom: 20%;
+            width: 2px;
+            background-color: var(--white);
+        }
+        &::before {
+            left: -2px;
+        }
+        &::after {
+            right: -2px;
+        }
+        .number {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    }
 }
 </style>
