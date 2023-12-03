d037a1f1affeadb372679428417c198315986408
정동묵 <siderid94@gmail.com>
2023-12-03 13:57:01 +0900

    환경변수 구성 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import dotenv from 'dotenv'
-dotenv.config()
-
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-            '@desktop': fileURLToPath(new URL('./src/components/desktop', import.meta.url)),
-            '@mobile': fileURLToPath(new URL('./src/components/mobile', import.meta.url)),
-            '@hybrid': fileURLToPath(new URL('./src/components/hybrid', import.meta.url)),
-            '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-            '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
-            '@common': fileURLToPath(new URL('./src/common', import.meta.url))
-        }
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: process.env.VITE_API_HOST, // ottol-server
-                changeOrigin: true, //CORS 방지
-                //api root 해당경로로 재탐색
-                rewrite: (path) => path.replace(/^\/api/, '/api')
+export default (mode: string) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+    return defineConfig({
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+                '@desktop': fileURLToPath(new URL('./src/components/desktop', import.meta.url)),
+                '@mobile': fileURLToPath(new URL('./src/components/mobile', import.meta.url)),
+                '@hybrid': fileURLToPath(new URL('./src/components/hybrid', import.meta.url)),
+                '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+                '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+                '@common': fileURLToPath(new URL('./src/common', import.meta.url))
+            }
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: process.env.VITE_API_HOST, // ottol-server
+                    changeOrigin: true, //CORS 방지
+                    //api root 해당경로로 재탐색
+                    rewrite: (path) => path.replace(/^\/api/, '/api')
+                }
             }
         }
-    }
-})
+    })
+}
