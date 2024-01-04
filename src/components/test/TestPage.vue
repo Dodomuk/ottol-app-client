3defa872cfe1bad2f2224532c9ea73158472544a
정동묵 <siderid94@gmail.com>
2024-01-04 22:15:04 +0900

    gsap 적용 여부 테스트

--- src/components/test/TestPage.vue
+++ src/components/test/TestPage.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>테스트 중 입니다.</div>
+    <body>
+        <div>테스트 중 입니다.</div>
+        <div class="test-circle">테스트</div>
+    </body>
 </template>
 <script lang="ts">
-export default {}
+import gsap from 'gsap'
+
+gsap.to('.test-circle', { rotation: 360, x: 100, duration: 1 })
 </script>
-<style lang="scss"></style>
+<style scoped lang="scss">
+body {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    min-height: 100vh;
+    flex-direction: column;
+    background: #444;
+}
+.test-circle {
+    width: 100px;
+    height: 100px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: aqua;
+}
+</style>
